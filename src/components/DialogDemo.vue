2c21472d1066b0ee87865f5b05d62b2dd54ab093
yyouangy <kk450512574@163.com>
2021-03-08 18:06:38 +0800

    创建kDialog组件

---
+++ src/components/DialogDemo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>dialog示例</div>
+  <h1>示例1</h1>
+  <k-dialog />
+</template>
+
+<script lang='ts'>
+import kDialog from "../lib/kDialog.vue";
+export default {
+  components: {
+    kDialog,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/lib/kDialog.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="dialog-mask"></div>
+  <div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>第一行字</p>
+      <p>第二行字</p>
+    </main>
+    <footer>
+      <k-button>OK</k-button>
+      <k-button>Cancel</k-button>
+    </footer>
+  </div>
+</template>
+
+<script>
+import kButton from "./kButton.vue";
+export default {
+  components: {
+    kButton,
+  },
+};
+</script>
+
+<style>
+</style>
